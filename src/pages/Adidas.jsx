ffd79095de5257ad34a6fbac14dc17a92cfa17e9
Caleb <caleb542@gmail.com>
2023-02-17 16:18:54 -0500

    working on the nav 2.1

---
+++ src/pages/Adidas.jsx
@@ -0,0 +1,39 @@
+function adidas() {
+  return (
+    <div className="adidas">
+        <div className="container">
+            <h1>
+               
+            </h1>
+            <div className="logo">
+                <div className="flower">
+                    <div className="petal">
+                        <div className="half1"></div>
+                        <div className="half2"></div>
+                    </div>
+                    <div className="petal">
+                        <div className="half1"></div>
+                        <div className="half2"></div>
+                    </div>
+                    <div className="petal">
+                        <div className="half1"></div>
+                        <div className="half2"></div>
+                    </div>
+                    <div className="bars">
+                        <div className="bar"></div>
+                        <div className="bar"></div>
+                        <div className="bar"></div>
+                    </div>
+
+                </div>
+      
+                     <h2 className='adidas'>adidas</h2>
+              
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default adidas
---
+++ src/pages/Flowers.jsx
@@ -0,0 +1,23 @@
+
+
+function Flowers() {
+  return (
+    <div className="circles">
+        <div className="container">
+            <h1><span>Spi</span>nning F<span>lo</span><span>wer</span>s</h1>
+ 
+        <div className="logo">
+            <div className="circle-container">
+                <div className="circle circle1"></div>
+                <div className="circle circle2"></div>
+                <div className="circle circle3"></div>
+                <div className="circle circle4"></div>
+
+            </div>
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default Flowers
